512f027b5e159e38c5893b4b35e25f78e60329cb
daulet2030 <daulet2030@gmail.com>
2020-12-29 06:51:22 +0600

    day 5, selenium intro

--- src/day05/SeleniumIntro.java
+++ src/day05/SeleniumIntro.java
@@ -1,5 +1,7 @@
 package day05;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntro {
@@ -7,5 +9,10 @@ public class SeleniumIntro {
         System.setProperty("webdriver.chrome.driver", "path-to-chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get( "https://techno.study/" );
+        driver.manage().window().maximize();
+
+        WebElement element = driver.findElement(By.cssSelector(".et_pb_text_inner"));
+        String text = element.getText();
+        System.out.println(text);
     }
 }
