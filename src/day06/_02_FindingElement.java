6d0d8cd308d268166f7d3125d471bff05d33a4a6
daulet2030 <daulet2030@gmail.com>
2020-12-30 06:37:29 +0600

    day 6, find by id

---
+++ src/day06/_02_FindingElement.java
@@ -0,0 +1,19 @@
+package day06;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _02_FindingElement {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://www.seleniumeasy.com/test/" );
+
+        WebElement button = driver.findElement(By.id("btn_basic_example"));
+        String buttonText = button.getText();
+        System.out.println(buttonText);
+
+        driver.quit();
+    }
+}
