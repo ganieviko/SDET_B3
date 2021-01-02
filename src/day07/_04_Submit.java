c2a6285b3a0ffa89bbc76603f6c93883a25e4eea
daulet2030 <daulet2030@gmail.com>
2021-01-02 06:51:20 +0600

    day 7, submit

---
+++ src/day07/_04_Submit.java
@@ -0,0 +1,18 @@
+package day07;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _04_Submit {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://www.seleniumeasy.com/test/basic-first-form-demo.html" );
+
+//        driver.findElement(By.id("at-cv-lightbox-close")).click();
+        driver.findElement(By.id("user-message")).sendKeys("Hello");
+        driver.findElement(By.id("get-input")).submit();
+
+//        driver.quit();
+    }
+}
