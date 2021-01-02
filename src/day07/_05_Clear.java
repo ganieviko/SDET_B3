91ea259e0e9cb41c6701d2111a38ddee999aeaf7
daulet2030 <daulet2030@gmail.com>
2021-01-02 07:16:20 +0600

    day 7, clear

---
+++ src/day07/_05_Clear.java
@@ -0,0 +1,21 @@
+package day07;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _05_Clear {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://www.seleniumeasy.com/test/basic-first-form-demo.html" );
+
+//        driver.findElement(By.id("at-cv-lightbox-close")).click();
+        driver.findElement(By.id("user-message")).sendKeys("Hello");
+
+        Thread.sleep(5000); // wait for 2 seconds
+
+        driver.findElement(By.id("user-message")).clear();
+
+//        driver.quit();
+    }
+}
