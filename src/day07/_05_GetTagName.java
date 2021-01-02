42b46f1ca4815b084dffc49c6e6e6f93ee2764fa
daulet2030 <daulet2030@gmail.com>
2021-01-02 07:19:33 +0600

    day 7, tag name

---
+++ src/day07/_05_GetTagName.java
@@ -0,0 +1,21 @@
+package day07;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _05_GetTagName {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://www.seleniumeasy.com/test/basic-first-form-demo.html" );
+
+        WebElement element = driver.findElement(By.id("user-message"));
+
+        String tagName = element.getTagName();
+
+        System.out.println(tagName);
+
+        driver.quit();
+    }
+}
