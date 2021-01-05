e6305c102d163f9f5badece4700ea39aba7e3ab3
daulet2030 <daulet2030@gmail.com>
2021-01-05 06:22:29 +0600

    day 8, get not present element with thread sleep

---
+++ src/day08/_03_GetNotPresentElementThreadSleep.java
@@ -0,0 +1,20 @@
+package day08;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _03_GetNotPresentElementThreadSleep {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "http://the-internet.herokuapp.com/dynamic_loading/2" );
+
+        driver.findElement(By.cssSelector("#start > button")).click();
+        Thread.sleep(5000);
+
+        String finish = driver.findElement(By.id("finish")).getText();
+        System.out.println(finish);
+
+        driver.quit();
+    }
+}
