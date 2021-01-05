4706c6e0a4a9c2c07f8bbff399980d03c115a101
daulet2030 <daulet2030@gmail.com>
2021-01-05 08:39:15 +0600

    day 8, JS prompt

---
+++ src/day08/_10_SelectByClicking.java
@@ -0,0 +1,28 @@
+package day08;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class _10_SelectByClicking {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/input-form-demo.html");
+
+//        driver.findElement(By.cssSelector("select[name=state] > :nth-child(3)")).click();
+
+        List<WebElement> options = driver.findElements(By.cssSelector("select[name=state] > option"));
+
+        for (int i = 1; i <= options.size(); i++) {
+            if(options.get(i).getText().equals("Mississippi")) {
+                options.get(i).click();
+            }
+        }
+
+//        driver.quit();
+    }
+}
