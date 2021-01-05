34b9bf1f5122455a54c4ddd8889aae04540322c0
daulet2030 <daulet2030@gmail.com>
2021-01-05 06:15:39 +0600

    day 8, get not present element

---
+++ src/day08/_02_GetNotPresentElement.java
@@ -0,0 +1,19 @@
+package day08;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _02_GetNotPresentElement {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "http://the-internet.herokuapp.com/dynamic_loading/2" );
+
+        driver.findElement(By.id("start")).click();
+
+        String finish = driver.findElement(By.id("finish")).getText();
+        System.out.println(finish);
+
+        driver.quit();
+    }
+}
