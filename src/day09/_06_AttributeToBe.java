44b259011c0c6b22a795a197059c686902e5fc81
daulet2030 <daulet2030@gmail.com>
2021-01-06 07:47:46 +0600

    day 9, attribute to be TODO

---
+++ src/day09/_06_AttributeToBe.java
@@ -0,0 +1,32 @@
+package day09;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class _06_AttributeToBe {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://css-tricks.com/examples/DynamicPage/#index.php" );
+
+        long currentTime = System.currentTimeMillis();
+        String style;
+        for (int i = 0; i < 50; i++) {
+            style = driver.findElement(By.id("main-content")).getAttribute("style");
+            System.out.println(style + " time: " + (System.currentTimeMillis() - currentTime));
+        }
+
+        driver.findElement(By.cssSelector("[href=\"about.php\"]")).click();
+        System.out.println("Clicked");
+
+        // TODO: show fluent wait, need to check every 100 milliseconds
+        for (int i = 0; i < 50; i++) {
+            style = driver.findElement(By.id("main-content")).getAttribute("style");
+            System.out.println(style + " time: " + (System.currentTimeMillis() - currentTime));
+        }
+
+//        driver.quit();
+    }
+}
