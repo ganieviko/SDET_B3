8ab53fa6f980fc2292b9063129bb083d3b5cd841
daulet2030 <daulet2030@gmail.com>
2021-01-06 08:40:29 +0600

    day 9, task2 solution not logged in

---
+++ src/day09/task2/Task2NotLoggedIn.java
@@ -0,0 +1,32 @@
+package day09.task2;
+import day09.MyConstants;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Task2NotLoggedIn {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://test.campus.techno.study/" );
+        driver.findElement(By.cssSelector("input[data-placeholder=\"Username\"]")).sendKeys("daulet2030@gmail.com");
+        driver.findElement(By.cssSelector("input[data-placeholder=\"Password\"]")).sendKeys("WrongPassword@");
+        driver.findElement(By.cssSelector("button[aria-label=\"LOGIN\"]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        try {
+            wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("svg[data-icon=\"bars\"]")));
+            System.out.println("Logged in!");
+        } catch (Exception e) {
+            System.out.println("Not logged in!");
+        }
+//        try {
+//            driver.findElement(By.cssSelector("svg[data-icon=\"bars\"]"));
+//            System.out.println("Logged in!");
+//        } catch (Exception e) {
+//            System.out.println("Not logged in!");
+//        }
+    }
+}
