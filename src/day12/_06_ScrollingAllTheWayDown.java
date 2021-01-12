3be821f973a7c3f72cd85c057b00ac288f34825e
daulet2030 <daulet2030@gmail.com>
2021-01-12 08:43:03 +0600

    day11, scrolling bottom

---
+++ src/day12/_06_ScrollingAllTheWayDown.java
@@ -0,0 +1,18 @@
+package day12;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _06_ScrollingAllTheWayDown {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://techno.study/");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+
+    }
+}
